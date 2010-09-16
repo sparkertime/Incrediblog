f508eb68d8b017bf0b999c60208cd06ce858ae49
Scott Parker <scott.p.parker@gmail.com>
2010-09-16 13:07:34 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Incrediblog::Application.config.secret_token = '3350a057389340c08d5dc036b183e599a59d676d39647767e46777fae8d82ca62a2a954a091a29b6a587c949664c3121c7ebfcc1079ab752ce3fbc420af67f3f'
