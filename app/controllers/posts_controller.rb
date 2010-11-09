ed0cb63fc96e18ad124487179545c2978078c6ea
Scott Parker <scott.p.parker@gmail.com>
2010-11-08 23:37:21 -0600

    Added pagination to PostsController#index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.xml
   def index
-    @posts = Post.all
+    @posts = Post.paginate :page => params[:page], :order => 'created_at DESC' 
 
     respond_to do |format|
       format.html # index.html.erb
