af4ebdfeeaf23c7db86808af89daa16bb49fd5e7
Scott Parker <scott.p.parker@gmail.com>
2010-12-21 08:41:55 -0600

    Limited the number of posts shown at posts#index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,9 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.xml
   def index
-    @posts = Post.paginate :page => params[:page], :order => 'created_at DESC' 
+    @posts = Post.paginate :page => params[:page],
+      :order => 'created_at DESC',
+      :per_page => 7
 
     respond_to do |format|
       format.html # index.html.erb
