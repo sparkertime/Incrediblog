5a2503e56210b7c14a796a07192d9df57c728def
Scott Parker <scott.p.parker@gmail.com>
2011-01-03 12:46:08 -0600

    Finished up the SiteMailer

--- app/mailers/site_mailer.rb
+++ app/mailers/site_mailer.rb
@@ -2,6 +2,8 @@ class SiteMailer < ActionMailer::Base
   default :from => "no-reply@citizenparker.com", :to => AppConfig.owner_email
 
   def contact_email(contact_options)
+    @contact_options = contact_options
+
     mail(
          :subject => "[#{AppConfig.web_host}] Message from #{contact_options[:sender_name]}"
         )
