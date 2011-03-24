fe6ccd8d9aa3dd44d4419a732f097e22fb1fe637
Scott Parker <scott.p.parker@gmail.com>
2011-03-23 21:14:35 -0500

    Cleaned up the syntax plugin

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source :rubygems
 
+gem "duostack"
 gem "jekyll"
 gem "haml"
+gem "hpricot"
+gem "httparty"
 gem "rack", "<= 1.2.1"
 gem "rack-jekyll"
---
+++ _plugins/syntax.rb
@@ -0,0 +1,24 @@
+require 'haml'
+require 'haml/html'
+require 'httparty'
+
+module Jekyll
+  class SyntaxBlock < Liquid::Block
+    def initialize(tag_name, language, tokens)
+      super
+      @language = language
+    end
+
+    def render(context)
+      Haml::HTML.new(code_to_html(super.join,@language)).render
+    end
+
+    def code_to_html(code, language)
+      response = HTTParty.post('http://pygments.appspot.com/'), :body => {'lang'=>language.strip, 'code'=>code})
+      puts "An error occured while processing this #{language} code: #{code}" unless response.success?
+      request.body
+    end
+  end
+end
+
+Liquid::Template.register_tag('syntax', Jekyll::SyntaxBlock)
