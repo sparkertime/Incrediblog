b8c5bfa1e13cf75e2cd23676ae63620f98a20ca6
Scott Parker <scott.p.parker@gmail.com>
2011-04-01 23:45:50 -0500

    New theme. Better.

--- _images/support/regen_stamp.sh
+++ _images/support/regen_stamp.sh
@@ -1,15 +1,11 @@
 convert -size 130x30 xc:grey30 -font Helvetica -pointsize 16 -gravity center \
-        -draw "fill grey10  text 0,0  'citizenparker.com'" \
+        -draw "fill grey10  text 0,0  'spparker.com'" \
         stamp_fgnd.png
 convert -size 130x30 xc:black -font Helvetica -pointsize 16 -gravity center \
-        -draw "fill white  text  1,1  'citizenparker.com'  \
-                           text  0,0  'citizenparker.com'  \
-               fill black  text -1,-1 'citizenparker.com'" \
+        -draw "fill white  text  1,1  'spparker.com'  \
+                           text  0,0  'spparker.com'  \
+               fill black  text -1,-1 'spparker.com'" \
         +matte stamp_mask.png
 composite -compose CopyOpacity  stamp_mask.png  stamp_fgnd.png  stamp.png
 mogrify -trim +repage stamp.png
-composite -gravity southeast stamp.png uwars.jpg output.jpg
-convert output.jpg -border 10x10 uwars_web.jpg
-rm output.jpg
-rm stamp*.*
-open uwars_web.jpg
+rm stamp_*.*
