2299f7077bdbd598a1408edd56fbf2f9e3f64867
Scott Parker <scott.p.parker@gmail.com>
2011-05-11 23:29:26 -0500

    Minor cleanup to email sending.

--- lib/email.rb
+++ lib/email.rb
@@ -18,9 +18,8 @@ class Emailer < Sinatra::Base
         :enable_starttls_auto => true,
         :user_name => ENV['EMAIL_USERNAME'],
         :password => ENV['EMAIL_PASSWORD'],
-        :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
-        #:domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
-        :domain => 'spparker.com',
+        :authentication       => :plain,
+        :domain => 'spparker.com'
       }
     )
 
